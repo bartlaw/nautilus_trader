031c0ecca4781b4d4a79f47740d2b7b13e45f291
bartlaw <barlaw@gmail.com>
2025-06-16 22:03:17 +0200

    fixed IBKR partially filled bracket order bug

--- nautilus_trader/adapters/interactive_brokers/execution.py
+++ nautilus_trader/adapters/interactive_brokers/execution.py
@@ -759,6 +759,12 @@ class InteractiveBrokersExecutionClient(LiveExecutionClient):
                 if order.totalQuantity == UNSET_DECIMAL
                 else Quantity.from_str(str(order.totalQuantity))
             )
+
+            if total_qty <= 0.0:
+                # This can be triggered by partially filled bracket orders and SL triggered.
+                self._log.warning(f"IB order with totalQuantity <= 0, skipping: {order.__dict__}")
+                return
+
             price = (
                 None if order.lmtPrice == UNSET_DOUBLE else instrument.make_price(order.lmtPrice)
             )
